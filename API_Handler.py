df70a1b8a42e12a1ae208a738ce1f77444453960
David B. Maier <davidbmaier@de.ibm.com>
2020-09-19 13:05:21 +0200

    Add logic to fetch TOTD track

--- API_Handler.py
+++ API_Handler.py
@@ -19,11 +19,15 @@ login_bytes = login.encode('ascii')
 base64_bytes = base64.b64encode(login_bytes)
 base64_login = base64_bytes.decode('ascii')
 
-level_zero_params = {
-    "Authorization": 'Basic ' + base64_login,
+default_headers = {
     "Content-Type": 'application/json',
     "Ubi-AppId": "86263886-327a-4328-ac69-527f0d20a237",
     "Ubi-RequestedPlatformType": "uplay"
+}
+
+level_zero_params = {
+    **default_headers,
+    "Authorization": 'Basic ' + base64_login
 }
 
 level_zero = requests.post(
@@ -34,6 +38,7 @@ level_zero = requests.post(
 ticket = level_zero["ticket"]
 
 level_one_params = {
+    **default_headers,
     "Authorization": "ubi_v1 t=" + ticket
 }
 
@@ -42,11 +47,12 @@ level_one = requests.post(
     headers=level_one_params
 ).json()
 
-access_token = level_one["accessToken"]
-refresh_token = level_one["refreshToken"]
+access_token_ubi = level_one["accessToken"]
+refresh_token_ubi = level_one["refreshToken"]
 
+# using the default headers here seems to result in a malformed JSON in the API response?
 level_two_params = {
-    "Authorization": "nadeo_v1 t=" + access_token
+    "Authorization": "nadeo_v1 t=" + access_token_ubi
 }
 
 level_two = requests.post(
@@ -55,4 +61,27 @@ level_two = requests.post(
     headers=level_two_params
 ).json()
 
-jprint(level_two)
+access_token_nadeo = level_two['accessToken']
+refresh_token_nadeo = level_two['refreshToken']
+
+# get the current month of TOTDs
+totd = requests.get(
+    "https://live-services.trackmania.nadeo.live/api/token/campaign/month",
+    headers={"Authorization": "nadeo_v1 t=" + access_token_nadeo}
+).json()
+
+# get the current TOTD from the list
+current_track_meta = {}
+for track in totd["monthList"][0]["days"]:
+    if track["relativeStart"] < 0 and track["relativeEnd"] > 0:
+        current_track_meta = track
+        break
+
+# get the current TOTD map data
+current_track = requests.get(
+    "https://prod.trackmania.core.nadeo.online/maps/?mapUidList=" +
+    current_track_meta["mapUid"],
+    headers={"Authorization": "nadeo_v1 t=" + access_token_ubi}
+).json()
+
+jprint(current_track)
